e23e982bdc1ea81badfa0c32545d085a9d9d808b
srimani-programmer <srimani.scholar@gmail.com>
2019-08-22 18:18:23 +0530

    Final Commit for Course03.

---
+++ Course_03/APIS/Assignment01.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+import json
+
+url = 'http://py4e-data.dr-chuck.net/comments_190247.json'
+req = urlopen(url).read()
+
+jsonHandler = json.loads(req)
+
+dataList = jsonHandler['comments']
+
+total_sum = 0
+
+for i in dataList:
+    total_sum += int(i['count'])
+
+print(total_sum)
